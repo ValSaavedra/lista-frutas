d128f3fbeb66b62004d419b830fea6bdfe7504d4
ValSaavedra <val.dicion@gmail.com>
2017-04-20 17:59:47 -0300

    Desarrollo del ejercicio solo falta imagen

---
+++ assets/js/javascript.js
@@ -0,0 +1,11 @@
+var texto = "<strong>Frutas Master<strong> es una empresa familiar de tercera generación que se dedica a la compra, venta y distribución de frutas al por mayor.<br> En la actualidad, somos la empresa líder en el servivio de distribución de frutas.";
+var catálogo "<strong>Catálogo<strong>";
+
+"1.Manzana<br>2.Pera<br>3.Piña<br>4.Cereza<br>5.Naranja";
+
+/*var boton = document.createElement("Agregar");
+boton.type = "button";
+document.body.appendChild(boton); */
+function boton(){
+  alert("frutas")
+  }
